a5bff24222b5e8aef7794777cff64bec724fad0f
Violet Kim <violetshirokuma@gmail.com>
2019-12-19 13:51:45 -0800

    Added screenshot, need to edit ReadMe

--- script.js
+++ script.js
@@ -1,63 +1,78 @@
-const specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-const numericSet = "0123456789";
-const lowerSet = "abcdefghijklmnopqrstuvwxyz";
-const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specialChar = "!#$%&'()*+,-./:;<>?@[]^_`{|}~";
+const numOption = "0123456789";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function generate() {
   let userInput = prompt("Please input a number between 8 - 128 for length of password.");
-  let userSize = parseInt(userInput);
-  let specialOption = confirm("Would you like your password to contain SPECIAL CHARACTERS? (!@#$%^&)");
-  let numericOption = confirm("Would you like your password to contain NUMBERS? (1,2,4,5,6...)");
-  let lowerOption = confirm("would you like your password to contain LOWER CASE LETTERS? (a,b,c,d,e...)");
-  let upperOption = confirm("would you like your password to contain UPPER CASE LETTERS? (A,B,C,D,E...)");
+  let passwordSize = parseInt(userInput);
+
+  //dialogues for characters and options
+  let specialOption = confirm("Would you like the password to have SPECIAL CHARACTERS? (!@#$%^&)");
+  let numericOption = confirm("Would you like the password to have NUMBERS? (1,2,4,5,6...)");
+  let lowerOption = confirm("Would you like the password to have LOWER CASE LETTERS? (a,b,c,d,e...)");
+  let upperOption = confirm("Would you like the password to have UPPER CASE LETTERS? (A,B,C,D,E...)");
 
   let passwordResult = "";
-  let charsConfirmed = "";
+  let charConfirm = "";
 
-  if (userSize < 8 || userSize > 128 || isNaN(userSize)) {
-    alert("You must enter a number between 8 to 128 characters.");
+  
+  if (passwordSize < 8 || passwordSize > 128 || isNaN(passwordSize)) {
+    alert("You must enter a number between 8 to 128 characters in order to generate a password.");
     return;
 
   }
    if (specialOption) {
-    charsConfirmed += specialChar;
+    charConfirm += specialChar;
   }
 
   if (numericOption) {
-    charsConfirmed += numericSet;
+    charConfirm += numOption;
   }
   if (lowerOption) {
-    charsConfirmed += lowerSet;
+    charConfirm += lowerCase;
   }
 
   if (upperOption) {
-    charsConfirmed += upperSet;
+    charConfirm += upperCase;
   }
+
+
+
   else {
-    if(charsConfirmed < 1){
+    if(charConfirm < 1){
      alert("You must choose one of the given character options.");
     }
   }
 
 
-  passLength = charsConfirmed.length;
+  passLength = charConfirm.length;
+
+
+
+
+
+// FUNCTION THAT CREATES THE PASSWORD BASED ON CRITERIA SELECTED ABOVE
 
 function randomize(length) {
     for (let i = 0; i <= length; i++) {
-      passwordResult += charsConfirmed.charAt(
+      passwordResult += charConfirm.charAt(
         Math.floor(Math.random() * passLength)
       );
     }
     return passwordResult;
   }
-  document.getElementById("password").innerHTML = randomize(userSize - 1);
+  document.getElementById("password").innerHTML = randomize(passwordSize - 1);
 }
 
+
+
+//COPY TO CLIPYBOARD BUTTON FUNCTION
 
 function copyButton() {
   var copyText = document.getElementById("password");
   copyText.select();
   copyText.setSelectionRange(0, 99999)
   document.execCommand("copy");
-  alert("Copied the text: " + copyText.value);
+  alert("The following text has been copied as your password: " + copyText.value);
 }
